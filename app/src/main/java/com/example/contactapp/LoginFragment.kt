c9d014616e0d4202a3ccb714319104d581960b08
Sbike <leroyhubert@yahoo.fr>
2023-12-08 16:57:59 +0100

    unused imports removed

--- app/src/main/java/com/example/contactapp/LoginFragment.kt
+++ app/src/main/java/com/example/contactapp/LoginFragment.kt
@@ -3,9 +3,7 @@ package com.example.contactapp
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
--- app/src/main/java/com/example/contactapp/ShowContactFragment.kt
+++ app/src/main/java/com/example/contactapp/ShowContactFragment.kt
@@ -4,9 +4,7 @@ import android.graphics.Bitmap
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -22,13 +20,13 @@ class ShowContactFragment : Fragment(R.layout.fragment_show_contact) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         imageView = view.findViewById(R.id.imageViewShowContact)
-        // Récupérer les arguments avec les données du contact
 
+        // Get the selected contact from the arguments
         val args: ShowContactFragmentArgs by navArgs()
         val selectedContact = args.selectedContact
         Log.d("Contact sélectionné", selectedContact.toString())
 
-        // Utiliser les données pour mettre à jour l'interface utilisateur
+        //Updating user interface
         updateUI(selectedContact)
         val btnDeleteContact = view.findViewById<Button>(R.id.btnDelete)
         btnDeleteContact.setOnClickListener {
@@ -36,6 +34,7 @@ class ShowContactFragment : Fragment(R.layout.fragment_show_contact) {
             findNavController().navigateUp()
         }
 
+        //Get the list of contacts
         val getBackContact = view.findViewById<Button>(R.id.btnGetBack)
         getBackContact.setOnClickListener {
             findNavController().navigateUp()
@@ -43,8 +42,6 @@ class ShowContactFragment : Fragment(R.layout.fragment_show_contact) {
     }
 
     private fun updateUI(contact: Contact) {
-        // Mettez à jour l'interface utilisateur avec les détails du contact (nom, prénom, numéro de téléphone, image, etc.)
-
         // Update name and first name
         val nameTextView = view?.findViewById<TextView>(R.id.textViewShowName)
         nameTextView?.text = contact.name
@@ -56,12 +53,11 @@ class ShowContactFragment : Fragment(R.layout.fragment_show_contact) {
         val phoneNumberTextView = view?.findViewById<TextView>(R.id.textViewShowPhoneNumber)
         phoneNumberTextView?.text = contact.phoneNumber
 
-        // Display the selected image in the ImageView
+        //Display the selected image in the ImageView
         displaySelectedImage(contact.image)
     }
 
     private fun displaySelectedImage(imageBitmap: Bitmap?) {
-        // Display the selected image in the ImageView
         if (imageBitmap != null) {
             Glide.with(requireContext())
                 .load(imageBitmap)
