edfde7988db519c8131a276e9378de325aa612e6
Sbike <leroyhubert@yahoo.fr>
2023-12-12 11:21:54 +0100

    mise en forme des methodes de test

--- app/src/test/java/com/example/contactapp/ContactAdditionalTests.kt
+++ app/src/test/java/com/example/contactapp/ContactAdditionalTests.kt
@@ -14,28 +14,22 @@ class ContactAdditionalTests {
 
     @Test
     fun contactNotEqualTest() {
-        // Create two different Contact objects
         val contact1 = Contact("Doe", "John", "1234567890", null)
         val contact2 = Contact("Smith", "Jane", "0987654321", null)
-
-        // Verify that the two objects are not equal
+        //we verify that the two contacts are not equal
         assertNotEquals(contact1, contact2)
     }
     @Test
     fun contactEqualsTest() {
-        // Create two identical Contact objects
         val contact1 = Contact("Doe", "John", "1234567890", null)
         val contact2 = Contact("Doe", "John", "1234567890", null)
-
-        // Verify that the two objects are equal
+        //We verify that the two contacts are equal
         assertEquals(contact1, contact2)
     }
     @Test
     fun contactPropertiesTest() {
-        // Create a Contact object
         val contact = Contact("Doe", "John", "1234567890", null)
-
-        // Verify that the properties are set correctly
+        //we verify that the properties are set correctly
         assertEquals("Doe", contact.name)
         assertEquals("John", contact.firstname)
         assertEquals("1234567890", contact.phoneNumber)
@@ -43,31 +37,22 @@ class ContactAdditionalTests {
     }
     @Test
     fun contactParcelableNotEqualTest() {
-        // Create two different Contact objects
         val originalContact = Contact("Doe", "John", "1234567890", null)
         val differentContact = Contact("Smith", "Jane", "0987654321", null)
-
-        // Mock the Parcel
         val parcel = mock(Parcel::class.java)
-
-        // Write the data of the original contact
+        //We write the data of the original contact
         originalContact.writeToParcel(parcel, originalContact.describeContents())
-
-        // After you're done with writing, you need to reset the parcel for reading
+        //We reset the parcel for reading
         parcel.setDataPosition(0)
-
-        // Read the data
+        //Reading the data
         val createdFromParcel = Contact.createFromParcel(parcel)
-
-        // Verify that the received data is not equal to the different contact
+        //We check that the received data is not equal to the different contact
         assertNotEquals(differentContact, createdFromParcel)
     }
     @Test
     fun contactDescribeContentsTest() {
-        // Create a Contact object
         val contact = Contact("Doe", "John", "1234567890", null)
-
-        // Call the method and verify the result
+        //We verify the method result
         assertEquals(0, contact.describeContents())
     }
 
--- app/src/test/java/com/example/contactapp/ContactViewModelAdditionalTests.kt
+++ app/src/test/java/com/example/contactapp/ContactViewModelAdditionalTests.kt
@@ -33,10 +33,9 @@ class ContactViewModelAdditionalTests {
     fun saveContactTest() = runBlockingTest {
         val contact = Contact("Doe", "John", "1234567890", null)
         viewModel.saveContact(contact)
-
         val observer = mock(Observer::class.java) as Observer<MutableList<Contact>>
         viewModel.listContacts.observeForever(observer)
-
+        //We verify that the contact has been saved
         assertEquals(1, viewModel.listContacts.value?.size)
         assertEquals(contact, viewModel.listContacts.value?.get(0))
     }
@@ -46,48 +45,23 @@ class ContactViewModelAdditionalTests {
         val contact = Contact("Doe", "John", "1234567890", null)
         viewModel.saveContact(contact)
         viewModel.deleteContact(contact)
-
         val observer = mock(Observer::class.java) as Observer<MutableList<Contact>>
         viewModel.listContacts.observeForever(observer)
-
+        //We verify that the contact has been deleted
         assertEquals(0, viewModel.listContacts.value?.size)
     }
-    /*
-    @Test
-    fun saveAndDeleteContactTest() = runBlockingTest {
-        // Create a Contact and save it
-        val contact = Contact("Doe", "John", "1234567890", null)
-        viewModel.saveContact(contact)
 
-        // Verify that the Contact was saved
-        val observer = mock(Observer::class.java) as Observer<MutableList<Contact>>
-        viewModel.listContacts.observeForever(observer)
-
-        assertEquals(1, viewModel.listContacts.value?.size)
-        assertEquals(contact, viewModel.listContacts.value?.get(0))
-
-        // Delete the Contact
-        viewModel.deleteContact(contact)
-
-        // Verify that the Contact was deleted
-        assertEquals(0, viewModel.listContacts.value?.size)
-    }
-     */
     @Test
     fun getSortedContactsTest() = runBlockingTest {
-        // Create and save multiple Contacts
         val contact1 = Contact("Doe", "John", "1234567890", null)
         val contact2 = Contact("Smith", "Jane", "0987654321", null)
         val contact3 = Contact("Brown", "Bob", "1122334455", null)
-
         viewModel.saveContact(contact1)
         viewModel.saveContact(contact2)
         viewModel.saveContact(contact3)
-
-        // Get the sorted list of Contacts
+        //We sort the contacts
         val sortedContacts = viewModel.getSortedContacts()
-
-        // Verify that the Contacts are sorted by firstname
+        //We verify that the contacts are sorted
         val sortedByFirstname = sortedContacts.sortedBy { it.firstname }
         assertEquals(sortedByFirstname, sortedContacts)
     }
